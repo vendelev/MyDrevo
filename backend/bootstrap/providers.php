e7bca845b876f9dff908d303f462526479eedfd3
Artiom <artiom@example.com>
2026-01-15 14:50:09 +0300

    Поправил autoload Убрал неактуальный код

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -2,10 +2,5 @@
 
 declare(strict_types=1);
 
-use Parser\Backup\Presentation\Config\BackupServiceProvider;
-use Parser\TicketDetail\Presentation\Config\TicketDetailServiceProvider;
-
 return [
-    BackupServiceProvider::class,
-    TicketDetailServiceProvider::class,
 ];
