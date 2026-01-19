6efc3ef6ebbeedfbadd8e12f1f9d6364cdc54fee
Artiom <artiom@example.com>
2026-01-19 18:27:30 +0300

    Буду делать всё заново

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -6,7 +6,6 @@ use App\Auth\Presentation\Config\AuthServiceProvider;
 use App\Core\Presentation\Config\CoreServiceProvider;
 use App\Core\Presentation\Config\RouteServiceProvider;
 use App\Example\Presentation\Config\ExampleServiceProvider;
-use App\Family\Presentation\Config\FamilyServiceProvider;
 use Illuminate\Auth\AuthServiceProvider as IlluminateAuthServiceProvider;
 use Illuminate\Cookie\CookieServiceProvider;
 
@@ -15,7 +14,6 @@ return [
     ExampleServiceProvider::class,
     RouteServiceProvider::class,
     AuthServiceProvider::class,
-    FamilyServiceProvider::class,
     IlluminateAuthServiceProvider::class,
     CookieServiceProvider::class,
 ];
